8ed7246cfad46544da8a1c8f19f76edf82de03a2
Yavuz Selim Çakmak <95148861+yavuzs-cakmak@users.noreply.github.com>
2026-06-10 19:35:20 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/MemuCalc/',
+  base: '/',
   test: {
     globals: true,
     environment: 'jsdom',
